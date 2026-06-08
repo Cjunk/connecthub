bcd88510ba07b092f51df58bc7934a23e74d035c
Jericho <jsharman@hotmail.com.au>
2026-06-08 19:49:35 +1000

    Normalize under construction include paths

--- under-construction.php
+++ under-construction.php
@@ -1,11 +1,11 @@
 <?php
-require_once 'config/constants.php';
-require_once 'config/bootstrap.php';
+require_once __DIR__ . '/config/constants.php';
+require_once __DIR__ . '/config/bootstrap.php';
 
 $pageTitle = 'Under Construction';
 ?>
 
-<?php include 'src/views/layouts/header.php'; ?>
+<?php include __DIR__ . '/src/views/layouts/header.php'; ?>
 
 <div class="container">
     <div class="row justify-content-center">
@@ -240,4 +240,4 @@ $pageTitle = 'Under Construction';
 }
 </style>
 
-<?php include 'src/views/layouts/footer.php'; ?>
+<?php include __DIR__ . '/src/views/layouts/footer.php'; ?>
