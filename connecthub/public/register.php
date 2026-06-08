9a86ea4e10a8733e65fa3ff087fa00013e6a5ae1
Jericho <jsharman@hotmail.com.au>
2026-06-08 17:13:42 +1000

    Remove nested duplicate project folder

--- connecthub/public/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config/constants.php';
-require_once 'config/bootstrap.php';
-// Don't include AuthController here - let the autoloader handle it
-
-$authController = new AuthController();
-$authController->showRegister();
