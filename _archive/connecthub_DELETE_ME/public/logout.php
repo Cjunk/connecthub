bb3ac353ebf362ca95d0a282f080b80cc9c8ad49
Jericho <jsharman@hotmail.com.au>
2026-06-08 19:25:36 +1000

    Stop tracking archived project files

--- _archive/connecthub_DELETE_ME/public/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config/constants.php';
-require_once 'config/bootstrap.php';
-
-session_destroy();
-setFlashMessage('success', 'You have been logged out successfully.');
-redirect(BASE_URL . '/index.php');
--- _archive/ordertracker.com.au/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('servername', 'localhost');
-define('username', 'jericho_user');
-define('password', 'password1password2');
-define('dbname', 'nez_balloons');
-?>
